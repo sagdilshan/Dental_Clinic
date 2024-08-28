71473f4c9a62ad107fdee2f01d42f49b94825680
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-08-28 18:12:39 +0530

    Update header.blade.php

--- resources/views/dashboard-layouts/header.blade.php
+++ resources/views/dashboard-layouts/header.blade.php
@@ -4,20 +4,19 @@
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-
-
     </ul>
+
+
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
 
 
-
-        <li class="nav-item">
+        {{-- <li class="nav-item">
             <a class="nav-link" href="{{ url('/') }}" role="button" title="Home">
                 <i class="nav-icon fa fa-home"></i>
             </a>
-        </li>
+        </li> --}}
 
         <li class="nav-item">
             <a class="nav-link" href="{{ route('admin.logout') }}" role="button" title="Log Out">
