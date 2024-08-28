60c6075b39d524bdbcffe83d1f4f1d497047ed6d
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-08-28 18:29:50 +0530

    Update patient-add.blade.php

--- resources/views/website-pages/admin/patient-add.blade.php
+++ resources/views/website-pages/admin/patient-add.blade.php
@@ -94,10 +94,10 @@
                                             </div>
 
                                             <div class="form-group row">
-                                                <label class="col-sm-2 col-form-label">Compain</label>
+                                                <label class="col-sm-2 col-form-label">Complain</label>
                                                 <div class="col-sm-10">
                                                     <textarea rows="2" class="form-control @error('complain') is-invalid @enderror" name="complain"
-                                                        placeholder="Compain"></textarea>
+                                                        placeholder="Complain"></textarea>
                                                     @error('complain')
                                                         <div class="invalid-feedback">
                                                             {{ $message }}
