e3a01df73c8de0d355ce0a1cdf3dbc386cde03d4
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-08-28 18:15:52 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Real Estate</title>
+    <title>Dental Clinic</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
